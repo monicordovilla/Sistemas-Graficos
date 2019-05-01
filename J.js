09f1039ceb31083a911d2beb9b8679510c6bc8b7
monicordovilla <32360865+monicordovilla@users.noreply.github.com>
2019-05-01 22:59:03 +0200

    Tetraminos J, L y O

---
+++ J.js
@@ -0,0 +1,31 @@
+//tetrimino I
+class J extends THREE.Mesh {
+	constructor() {
+		super();
+		var cuadrado = new THREE.BoxGeometry( 1, 1, 1 );
+		var esqueleto = new THREE.MeshBasicMaterial( {color: 0x000000, wireframe:true, blending: THREE.MultiplyBlending});
+		var azul = new THREE.MeshBasicMaterial( {color: 0x33cccc });
+
+		var cuadradobsp = new ThreeBSP (cuadrado);
+		var partialResult = cuadradobsp;
+
+		cuadrado.translate (1, 0, 0);
+		cuadradobsp = new ThreeBSP ( cuadrado ) ;
+		partialResult = partialResult.union ( cuadradobsp ) ;
+
+		for(var i=0; i<2; i++){
+		cuadrado.translate (0, 1, 0);
+		cuadradobsp = new ThreeBSP ( cuadrado ) ;
+		partialResult = partialResult.union ( cuadradobsp ) ;
+		}
+
+		this.material = azul;
+
+		this.result = partialResult.toMesh(this.material);
+		this.result.geometry.computeFaceNormals();
+		this.result.geometry.computeVertexNormals();
+
+    this.geometry = this.result.geometry;
+
+	}
+}
